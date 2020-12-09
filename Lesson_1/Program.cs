ac5386759eb1f4e306d551e7556baffd8acff2f9
Fert <ya.nibelungen@ya.ru>
2020-12-09 23:40:46 +0300

    Добавьте файлы проекта.

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now.ToString("dd.MM.yyyy")}");// Время не выводил, т.к. в задании просят вывести %дата%, я так понимаю для даты время не требуется.
+        }
+    }
+}
