f61a34fb0931db3acffaa751c2535c69a755c8ab
Fert <ya.nibelungen@ya.ru>
2020-12-23 23:39:24 +0300

    Домашнее задание, урок №4!

---
+++ Lesson_04/Ex_01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_01
+{
+    class Program
+    {
+        
+        static string GetFullName(string firstName, string patronymic, string lastName)
+        {
+            return $"Полное ФИО: {lastName} {firstName} {patronymic}";
+        }
+
+       
+        static void Main(string[] args)
+        {
+            for ( int i = 0; i < 4; i ++)
+            {
+                Console.WriteLine("Введите имя: ");
+                string firstName = Console.ReadLine();
+
+                Console.WriteLine("Введите отчество:");
+                string patronymic = Console.ReadLine();
+
+                Console.WriteLine("Введите фамилию:");
+                string lastName = Console.ReadLine();
+
+                string sGetFullName = GetFullName(firstName, patronymic, lastName);
+                Console.WriteLine(sGetFullName);
+            }
+             
+              
+            
+
+        }
+    }
+}
---
+++ Lesson_04/Ex_04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение последовательности вычисляющее число Фибоначчи:");
+            long n = Convert.ToInt64(Console.ReadLine());
+
+            long Number = Fibonacci(n);
+            Console.WriteLine(Number);
+        }
+        static long Fibonacci (long n)
+        {
+            if (n == 0 )
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+    }
+}
