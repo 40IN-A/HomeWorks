869a0b4a9d78a28cbdef1e52e28cec8b5ce1811b
Fert <ya.nibelungen@ya.ru>
2020-12-29 23:04:55 +0300

    Домашнее задание, урок №5. Упражнение №5 не сделал.

---
+++ Lesson_05/Ex_03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Введите с клавиатуры произвольный набор чисел (0...250): ");
+            string str = Console.ReadLine();
+            byte[] arr = str.Split(new char[] { ' ', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(n => Convert.ToByte(n)).ToArray();
+            FileStream file = new FileStream("Binari.dat", FileMode.Create, FileAccess.Write);
+            BinaryWriter tr = new BinaryWriter(file);
+            tr.Write(arr);
+            tr.Close();
+            file.Close();
+
+            //Чтение файла.
+
+            Console.WriteLine("Считываю массив из файла.");
+            if (File.Exists("Binari.dat")) //Проверяю, существует ли файл Binari.dot, если да, то начинаю считывать.
+            { 
+            FileStream fileR = new FileStream("Binari.dat", FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fileR);
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = br.ReadByte();
+            }
+            for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine($"{arr[i]}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson_05/Ex_04/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex_04
+{
+    class Program
+    {
+        static System.Collections.Specialized.StringCollection log = new System.Collections.Specialized.StringCollection();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сохранение дерева каталогов и файлов в файл.");
+            Console.WriteLine("Введите путь к каталогу:");
+            string dirName = Console.ReadLine();
+            if (Directory.Exists(dirName))
+            {
+                string heading1 = "Path.txt";
+                File.AppendAllText(heading1, $"\n Вложенные каталоги и файлы каталога {dirName}:");
+                string[] dirs = Directory.GetFileSystemEntries(dirName, "*", SearchOption.AllDirectories);
+                foreach (string s in dirs)
+                {
+                    string filename = "Path.txt";
+                    File.AppendAllText(filename, $"\n {s}");
+                }
+
+            }
+
+            Console.WriteLine("Сохранение дерева каталогов и файлов в файл с помощью рекурсии.");
+            Console.WriteLine("Введите путь к каталогу:");
+            string RdirName = Console.ReadLine();
+            string[] Rdirs = Directory.GetFiles(RdirName);//Здесь у меня ошибка, в зависимости от выбранного каталога меняется длинна массива, из-за этого запись дерева в файл происходит несколько раз.
+            foreach (string dr in Rdirs)                   //Как её обойти я так и не догадался.
+            {
+                DirectoryInfo di = new DirectoryInfo(dr);
+                DirectoryInfo rootDir = di.Parent;
+                string heading2 = "Path_Recursion.txt";
+                File.AppendAllText(heading2, $"\n Вложенные каталоги и файлы каталога {RdirName}:");
+                WalkDirectoryTree(rootDir);
+            }
+            
+            Console.WriteLine("Нажмите любую кнопку.");
+            Console.ReadKey();
+            
+
+
+
+        }
+        
+        static void WalkDirectoryTree(System.IO.DirectoryInfo root)
+        {
+            System.IO.FileInfo[] files = null;
+            System.IO.DirectoryInfo[] subDirs = null;
+
+            try
+            {
+                files = root.GetFiles("*.*");
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                log.Add(e.Message);
+            }
+
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (files != null)
+            {
+                foreach (System.IO.FileInfo fi in files)
+                {
+                    //Console.WriteLine(fi.FullName);
+                    string filename = "Path_Recursion.txt";
+                    File.AppendAllText(filename, $"\n {fi.FullName}");
+                }
+
+                subDirs = root.GetDirectories();
+
+                foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+                {
+                    WalkDirectoryTree(dirInfo);
+                    //Console.WriteLine(dirInfo.FullName);
+                    string filename = "Path_Recursion.txt";
+                    File.AppendAllText(filename, $"\n {dirInfo.FullName}");
+                }
+            }
+        }
+    }
+}
