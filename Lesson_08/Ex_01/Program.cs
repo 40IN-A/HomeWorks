31668b36bdbe4ae1b7cbee8dc432fdb5261902e5
Fert <ya.nibelungen@ya.ru>
2021-01-08 16:12:20 +0300

    Домашнее задание, урок №8.

---
+++ Lesson_08/Ex_01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(string.IsNullOrEmpty(Properties.Settings.Default.UserName))
+            {
+                Console.WriteLine("Введите имя пользователя:");
+                Properties.Settings.Default.UserName = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+
+            if(string.IsNullOrEmpty(Properties.Settings.Default.Age))
+            {
+                Console.WriteLine("Введите возраст:");
+                Properties.Settings.Default.Age = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+
+            if(string.IsNullOrEmpty(Properties.Settings.Default.SortOfActivity))
+            {
+                Console.WriteLine("Введите род деятельности:");
+                Properties.Settings.Default.SortOfActivity = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+
+            string userName = Properties.Settings.Default.UserName;
+            string age = Properties.Settings.Default.Age;
+            string sortOfActivity = Properties.Settings.Default.SortOfActivity;
+            string greeting = Properties.Settings.Default.Greeting;
+
+            Console.WriteLine($"{greeting}\n{sortOfActivity} {userName} {age} лет.");
+        }
+    }
+}
